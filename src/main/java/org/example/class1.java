1cdd5cb3f4f4fd5fce053482a4707f93f49004bf
SvetLanaLevL <lanabonitas@gmail.com>
2024-03-13 20:33:56 -0400

    second commit

--- src/main/java/org/example/class1.java
+++ src/main/java/org/example/class1.java
@@ -3,5 +3,9 @@ package org.example;
 public class class1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("hello mars");
+
+
     }
 }
